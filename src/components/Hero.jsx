8b830f9da08a1cf81a077aa3b1dff48f81c0c0bb
Kevin Geejo <74069952+KevinGeejo@users.noreply.github.com>
2024-01-22 19:11:07 +0530

    Update Hero component styling and content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className="bg-[#171717] text-white py-20 justify-center items-center">
-            <div className="container mx-auto px-4 flex flex-col items-center">
-                <h1 className="text-4xl font-bold mb-4 pt-40">Welcome to My Hero Page</h1>
-                <p className="text-lg mb-8">This is an example of a Hero component using Tailwind CSS.</p>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Get Started
-                </button>
+        <div className="container mx-auto max-w-5xl px-6 my-4">
+            <div className="bg-[#171717] text-white py-20 flex justify-center items-center">
+                <div className="flex flex-col items-center text-center">
+                    <h1 className="text-4xl font-bold mb-4 pt-[8%]  font-mono">Hi, I'm Kevin Geejo</h1>
+                    <p className="text-lg mb-8 mx-[20%] text-left">Exploring ways to create robust, scalable web Apps, Unlimitedly obsessed with solving problems where designing and engineering intersects, by creating pixel-perfect , polished interfaces. Currently tinkering with various projects. studying Computer Science, Interested in coding and improving consistently through learning. Currently interested in React Native and MERN stack.</p>
+                    <button className="bg-[#5bca9d] hover:bg-[#eeeeee] text-black font-bold py-3 px-8 rounded">
+                        Get Started
+                    </button>
+                </div>
             </div>
         </div>
     );
