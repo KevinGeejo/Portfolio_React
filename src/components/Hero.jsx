ed0e7b94bed4e78e6b20bf4b026a0aae4a1257ad
Kevin Geejo <74069952+KevinGeejo@users.noreply.github.com>
2024-01-26 00:40:08 +0530

    Refactor code to improve performance and readability

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
                 <div className="flex flex-col items-start text-left"> {/* Modified className */}
                     {/* <h1 className="text-4xl font-bold mb-4 pt-[7%]  font-mono">Hi, I'm Kevin Geejo. </h1> */}
                     <Typed className="text-5xl font-bold mb-4 pt-[7%]  font-mono" strings={['Hi!', "I'm Kevin Geejo."]} typeSpeed={60} backSpeed={60} ></Typed>
-                    <div className='container pr-80'>
-                    <p className="text-[19px] mb-8 pt-[25px] text-left">Exploring ways to create robust, scalable web Apps, Unlimitedly obsessed with solving problems where designing and engineering intersects, by creating pixel-perfect , polished interfaces. Currently tinkering with various projects. studying Computer Science, Interested in coding and improving consistently through learning. Currently interested in React Native and MERN stack.</p>
+                    <div className='container pr-[40%]'>
+                    <p className="text-[18px] mb-8 pt-[25px] text-left">Exploring ways to create robust, scalable web Apps, Unlimitedly obsessed with solving problems where designing and engineering intersects, by creating pixel-perfect , polished interfaces. Currently tinkering with various projects. studying Computer Science, Interested in coding and improving consistently through learning. Currently interested in React Native and MERN stack.</p>
 
                     </div>
                     {/* <button className="bg-[#5bca9d] hover:bg-[#eeeeee] text-black font-bold py-3 px-8 rounded">
