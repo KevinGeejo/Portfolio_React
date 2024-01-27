0e8efdbe6e3a73b6c0b234683d0eb39eb2884a8f
Kevin Geejo <74069952+KevinGeejo@users.noreply.github.com>
2024-01-28 00:50:27 +0530

    Update styling in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,10 @@ import bwImg from './assets/img/bw-img.png'; // Add this line
 const About = () => {
   return (
     <div className="w-full mt-80 max-w-6xl px-8 justify-center mx-auto">
-      <div className='bg-[#1e1e1f] rounded-sm mx-20 p-20'>
+      <div className='bg-[#1e1e1f] rounded-lg shadow-2xl	mx-20 p-20'>
 
-     <p className='text-white font-inter text-[30px] font-bold'>About Me</p>
+     <p className='text-[#fafafa] font-inter text-[30px] font-bold'>About Me</p>
+     <div className='bg-[#fdd26a] h-1 w-16 rounded-3xl mr-[70%]'><button></button></div>
       </div>
     </div>
   )
