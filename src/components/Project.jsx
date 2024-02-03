2d1c29ebd06d4478e7c3f7b8e54810d5346ac079
Kevin Geejo <74069952+KevinGeejo@users.noreply.github.com>
2024-02-03 16:26:07 +0530

    Refactor Project component layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import eye from '../assets/img/eye.png'
+import eye from '../assets/img/eye.png';
 
 const Project = () => {
   return (
@@ -13,29 +13,76 @@ const Project = () => {
         <div className="bg-[#fdd26a] h-1 w-16 rounded-3xl mr-[70%] mt-2">
           <button></button>
         </div>
-
-        <div className="flex flex-col w-[340px] h-[213px] bg-[#424242] mt-[105px] rounded-xl shadow-lg">
-          
-        
-            <div className='w-[267px] h-[115px] mt-[28px] ml-[28px] '>
-              <p className="text-[14px] text-[#FDD26A] font-bold">2023</p>
-              <h3 className="font-bold text-[#F5F4F4] mt-[10px] text-[16px]">
-                ClueNet : A NextJs Application
-              </h3>
-              <p className='text-[#A8A3A5] text-[13px] mt-[10px]'>
-                A real time Video chat , Texting & Screen Sharing Appliction created using NextJs and Firebase
-              </p>
-              <div className='flex justify-end'>
-              <button className='flex border-[1px]	w-[66px] h-[40px] rounded-xl justify-center border-[#fdd26a]'>
-                <a target='_blank' href="https://github.com/KevinGeejo/ClueNet" className="text-[#FDD26A] text-[14px] mt-[10px] font-bold">
-                  <img src={eye}>
-                  </img>
+    <div className='w-[710px] h-[456px] flex flex-wrap flex-row mt-[105px] justify-between'>
+        <div className="flex flex-col w-[340px] h-[213px] bg-[#424242] rounded-xl shadow-xlg">
+          <div className="w-[267px] h-[115px] mt-[28px] ml-[28px] ">
+            <p className="text-[14px] text-[#FDD26A] font-bold">2023</p>
+            <h3 className="font-bold text-[#F5F4F4] mt-[10px] text-[16px]">
+              ClueNet : A NextJs Application
+            </h3>
+            <p className="text-[#A8A3A5] text-[13px] mt-[10px]">
+              A real time Video chat , Texting & Screen Sharing Appliction
+              created using NextJs and Firebase
+            </p>
+            <div className="flex justify-end">
+              <button className="flex border-[1px]	w-[66px] h-[40px] rounded-xl justify-center border-[#fdd26a]">
+                <a
+                  target="_blank"
+                  href="https://github.com/KevinGeejo/ClueNet"
+                  className="text-[#FDD26A] text-[14px] mt-[10px] font-bold"
+                >
+                  <img src={eye}></img>
                 </a>
               </button>
-
-              </div>
             </div>
-         
+          </div>
+        </div>
+        <div className="flex flex-col w-[340px] h-[213px] bg-[#424242] rounded-xl shadow-xlg">
+          <div className="w-[267px] h-[115px] mt-[28px] ml-[28px] ">
+            <p className="text-[14px] text-[#FDD26A] font-bold">2023</p>
+            <h3 className="font-bold text-[#F5F4F4] mt-[10px] text-[16px]">
+              ClueNet : A NextJs Application
+            </h3>
+            <p className="text-[#A8A3A5] text-[13px] mt-[10px]">
+              A real time Video chat , Texting & Screen Sharing Appliction
+              created using NextJs and Firebase
+            </p>
+            <div className="flex justify-end">
+              <button className="flex border-[1px]	w-[66px] h-[40px] rounded-xl justify-center border-[#fdd26a]">
+                <a
+                  target="_blank"
+                  href="https://github.com/KevinGeejo/ClueNet"
+                  className="text-[#FDD26A] text-[14px] mt-[10px] font-bold"
+                >
+                  <img src={eye}></img>
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="mt-[30px] flex flex-col w-[340px] h-[213px] bg-[#424242] rounded-xl shadow-xlg">
+          <div className="w-[267px] h-[115px] mt-[28px] ml-[28px] ">
+            <p className="text-[14px] text-[#FDD26A] font-bold">2023</p>
+            <h3 className="font-bold text-[#F5F4F4] mt-[10px] text-[16px]">
+              ClueNet : A NextJs Application
+            </h3>
+            <p className="text-[#A8A3A5] text-[13px] mt-[10px]">
+              A real time Video chat , Texting & Screen Sharing Appliction
+              created using NextJs and Firebase
+            </p>
+            <div className="flex justify-end">
+              <button className="flex border-[1px]	w-[66px] h-[40px] rounded-xl justify-center border-[#fdd26a]">
+                <a
+                  target="_blank"
+                  href="https://github.com/KevinGeejo/ClueNet"
+                  className="text-[#FDD26A] text-[14px] mt-[10px] font-bold"
+                >
+                  <img src={eye}></img>
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
